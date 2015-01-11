b2c3a216ce135334499a603c6e0041c5e8e772c3
cmotc <cmotc@openmailbox.org>
2015-01-11 00:03:06 -0500

    basic decision tree in place

--- bin/config_wizard.sh
+++ bin/config_wizard.sh
@@ -1,2 +1,132 @@
-#!/bin/sh
-./var/fdg/variables.sh
+#!/bin/bash
+source var/fdg/variables.sh
+echo -e $INTRO
+read -p "Press any key to continue..." 
+#Configure architecture
+while true; do
+	read -p "$WANTMSM" yn
+	case $yn in
+		[Yy]* ) PDMSM="y"; break;;
+		[Nn]* ) PDMSM="n"; break;;
+		*) echo "Please answer Y/y or N/n";;
+	esac
+done
+	if [ [$PDMSM="y"] ];then
+		while true; do
+			read -p "$WANTMSM7XXXA" yn
+			case $yn in
+				[Yy]* ) PDS="y"; break;;
+				[Nn]* ) PDS="n"; break;;
+				*) echo "Please answer Y/y or N/n";;
+			esac
+		done
+			if [ [$PDS="n"] ]; then
+				while true; do
+					read -p "$WANTMSM7X30A" yn
+					case $yn in
+						[Yy]* ) PDSS="y"; break;;
+						[Nn]* ) PDSS="n"; break;;
+						*) echo "Please answer Y/y or N/n";;
+					esac
+				done
+			fi
+		while true; do
+			read -p "$WANTADRENO" yn
+			case $yn in
+				[Yy]* ) PDRENO="y"; break;;
+				[Nn]* ) PDRENO="n"; break;;
+				*) echo "Please answer Y/y or N/n";;
+			esac
+		done
+	fi
+#Configure Android
+while true; do
+	read -p "$WANTANDROID" yn
+	case $yn in
+		[Yy]* ) PDA="y"; break;;
+		[Nn]* ) PDA="n"; break;;
+		*) echo "Please answer Y/y or N/n";;
+	esac
+done
+	if [ [$PDA="y"] ]; then
+		while true; do
+			read -p "$WANTANDROIDBASE" yn
+			case $yn in
+				[Yy]* ) PDAB="y"; break;;
+				[Nn]* ) PDAB="n"; break;;
+				*) echo "Please answer Y/y or N/n";;
+			esac
+		done
+		while true; do
+			read -p "$WANTANDROIDFULL" yn
+			case $yn in
+				[Yy]* ) PDAF="y"; break;;
+				[Nn]* ) PDAF="n"; break;;
+				*) echo "Please answer Y/y or N/n";;
+			esac
+		done
+	fi
+# Configure Wi-Fi
+while true; do
+	read -p "$WANTWIFI" yn
+	case $yn in
+		[Yy]* ) PDWF="y"; break;;
+		[Nn]* ) PDWF="n"; break;;
+		*) echo "Please answer Y/y or N/n";;
+	esac
+done
+	if [ [$PDWF="y" ] ]; then
+		while true; do
+			read -p "$WANTATH6KL" yn
+			case $yn in
+				[Yy]* ) PDATHM="y"; break;;
+				[Nn]* ) PDATHM="n"; break;;
+				*) echo "Please answer Y/y or N/n";;
+			esac
+		done
+			if [ [$PDATHM="y"] ]; then
+				while true; do
+					read -p "$WANTATH6KLU" yn
+					case $yn in
+						[Yy]* ) PDATHMU="y"; break;;
+						[Nn]* ) PDATHMU="n"; break;;
+						*) echo "Please answer Y/y or N/n";;
+					esac
+				done
+				while true; do
+					read -p "$WANTATH6KLD" yn
+					case $yn in
+						[Yy]* ) PDATHMD="y"; break;;
+						[Nn]* ) PDATHMD="n"; break;;
+						*) echo "Please answer Y/y or N/n";;
+					esac
+				done
+			fi
+		while true; do
+			read -p "$WANTATHFOSS" yn
+			case $yn in
+				[Yy]* ) PDATH="y"; break;;
+				[Nn]* ) PDATH="n"; break;;
+				*) echo "Please answer Y/y or N/n";;
+			esac
+		done
+			if [ [$PDATH="y"] ]; then
+				while true; do
+					read -p "$WANTATHFOSSD" yn
+					case $yn in
+						[Yy]* ) PDATHD="y"; break;;
+						[Nn]* ) PDATHD="n"; break;;
+						*) echo "Please answer Y/y or N/n";;
+					esac
+				done
+			fi
+	fi
+# Configure SELinux
+while true; do
+	read -p "$WANTSELINUX" yn
+	case $yn in
+		[Yy]* ) PDSEL="y"; break;;
+		[Nn]* ) PDSEL="n"; break;;
+		*) echo "Please answer Y/y or N/n";;
+	esac
+done
--- var/fdg/variables.sh
+++ var/fdg/variables.sh
@@ -1,83 +1,131 @@
 #! /bin/bash
 source var/fdg/supported.sh
-ABD=$(<var/android/configs/android-base.cfg)
-AFD=$(<var/android/configs/android-recommended.cfg)
-INTRO="\n
-=================================================================================\n
-Generate Defconfig by feature\n
---------------------------------------------------------------------------------\n
-This is a tool for generating a Linux kernel defconfig using a wizard which asks \n
-a series of questions about which features you require. What follows is a series \n
-of question trees which will merge partial defconfig files with their \n
-dependencies resolved in-and-of themselves to output a finished product. \n 
-Obviously the variety of generated defconfigs cannot all be tested on all \n
-hardware and all kernel versions, and they cannot be fully supported all at the \n
-same time. This list of supported targets will be updated as I figure them out.\n\n
+ABD=$(<var/android/configs/android-base)
+AFD=$(<var/android/configs/android-recommended)
+INTRO="
+=================================================================================
+Generate Defconfig by feature
+--------------------------------------------------------------------------------
+This is a tool for generating a Linux kernel defconfig using a wizard which asks 
+a series of questions about which features you require. What follows is a series 
+of question trees which will merge partial defconfig files with their 
+dependencies resolved in-and-of themselves to output a finished product.  
+Obviously the variety of generated defconfigs cannot all be tested on all 
+hardware and all kernel versions, and they cannot be fully supported all at the 
+same time. This list of supported targets will be updated as I figure them out.
 $SUPPORTED
 "
-WANTMSM="\n
-For right now, this tool will only support Qualcomm hardware because \n
-Qualcomm is goddamned everywhere and it's cheap, which for me, makes it fun to \n
-hack on. But there are like, zillions of Qualcomm based configurations and it \n
+WANTMSM="
+For right now, this tool will only support Qualcomm hardware because 
+Qualcomm is goddamned everywhere and it's cheap, which for me, makes it fun to 
+hack on. But there are like, zillions of Qualcomm based configurations and it 
 can be confusing what needs to be set to make a SOC work with a particular stack
 and hence this tool. In the future, this prompt will be replaced with a list of
-minimal defconfigs by architecture.\n 
+minimal defconfigs by architecture. 
+Target the Qualcomm MSM architecture? 
 "
 WANTMSM7XXXA="
-================================================================================\n
-Is your target the Qualcomm Snapdragon S1?\n
-Flag(s) : (CONFIG_ARCH_MSM7X00A) \n
-Other Names : (MSM7225, MSM7625, MSM7227, MSM7627, MSM7225A, MSM7625A, MSM72271,\n
-\tMSM7627A,MSM7225AB, QSD8250, QSD8650)\n
---------------------------------------------------------------------------------\n
+================================================================================
+Is your target the Qualcomm Snapdragon S1(y/n)?
+Flag(s) : (CONFIG_ARCH_MSM7X00A) 
+Other Names : (MSM7225, MSM7625, MSM7227, MSM7627, MSM7225A, MSM7625A, MSM72271,
+\tMSM7627A,MSM7225AB, QSD8250, QSD8650)
+--------------------------------------------------------------------------------
 "
 WANTMSM7X30="
-================================================================================\n
-Is your target the Snapdragon S2?\n
-Flags(s) : (CONFIG_ARCH_MSM7X30) \n
-Other Names : (MSM7230, MSM7630, APQ8055, MSM8255, MSM8255T, MSM8655, MSM8655T)\n
---------------------------------------------------------------------------------\n
+================================================================================
+Is your target the Snapdragon S2(y/n)?
+Flags(s) : (CONFIG_ARCH_MSM7X30) 
+Other Names : (MSM7230, MSM7630, APQ8055, MSM8255, MSM8255T, MSM8655, MSM8655T)
+--------------------------------------------------------------------------------
+"
+WANTADRENO="
+================================================================================
+Do you want to configure the Adreno GPU?
+(y/n)?
+Flags(s) : (CONFIG_FB_MSM) 
+--------------------------------------------------------------------------------
 "
 WANTANDROID="
-================================================================================\n
-Do you want Android features enabled?\n
-Flags(s) : (follow-up) \n
+================================================================================
+Do you want Android features enabled(y/n)?
+Flags(s) : (follow-up) 
 You will be prompted to choose between the base or full Android defconfig
---------------------------------------------------------------------------------\n
+--------------------------------------------------------------------------------
 "
 WANTANDROIDBASE="
-================================================================================\n
-Do you just want the base android features?\n
-Flags(s) : ($ABD) \n
-Other Names : \n
---------------------------------------------------------------------------------\n
+================================================================================
+Do you just want the base android features(y/n)?
+Flags(s) : ($ABD) 
+Other Names : 
+--------------------------------------------------------------------------------
 "
 WANTANDROIDFULL="
-================================================================================\n
-Do you want the full Android features?\n
-Flags(s) : ($AFD) \n
---------------------------------------------------------------------------------\n
+================================================================================
+Do you want the full Android features(y/n)?
+Flags(s) : ($AFD) 
+--------------------------------------------------------------------------------
+"
+WANTWIFI="
+================================================================================
+Do you want to configure Wi-Fi Devices?
+(y/n)?
+Flags(s) : (One or more of the following: CONFIG_ATH9K_HTC, 
+CONFIG_ATH9K_HTC_DEBUGFS, CONFIG_CARL9170, CONFIG_CARL9170_LEDS, 
+CONFIG_CARL9170_WPC, CONFIG_ATH6KL, CONFIG_ATH6KL_SDIO, CONFIG_ATH6KL_USB, 
+CONFIG_ATH6KL_DEBUG) 
+--------------------------------------------------------------------------------
+"
+WANTATH6KL="
+================================================================================
+Do you want to configure the Atheros mobile chipset standard Version?
+(Non-Free firmware required.)
+(y/n)?
+Flags(s) : (CONFIG_ATH6KL, CONFIG_ATH6KL_SDIO) 
+--------------------------------------------------------------------------------
+"
+WANTATH6KLU="
+================================================================================
+Do you want to configure the Atheros mobile chipset USB Version?
+(Non-Free firmware required.)
+(y/n)?
+Flags(s) : (CONFIG_ATH6KL_USB) 
+--------------------------------------------------------------------------------
+"
+WANTATH6KLD="
+================================================================================
+Do you want to configure the Atheros mobile chipset debugging tools?
+(Non-Free firmware required.)
+(y/n)?
+Flags(s) : (CONFIG_ATH6KL_DEBUG) 
+--------------------------------------------------------------------------------
+"
+WANTATHFOSS="
+================================================================================
+Do you want to configure the Atheros 9k_htc USB chipset?
+(Respects your freedom.)
+(y/n)?
+Flags(s) : (CONFIG_ATH9K_HTC, CONFIG_CARL9170, CONFIG_CARL9170_LEDS, 
+CONFIG_CARL9170_WPC) 
+--------------------------------------------------------------------------------
+"
+WANTATHFOSSD="
+================================================================================
+Do you want to configure the Atheros 9k_htc USB chipset debugging tools?
+(Respects your freedom.)
+(y/n)?
+Flags(s) : (CONFIG_ATH9K_HTC_DEBUGFS) 
+--------------------------------------------------------------------------------
 "
 WANTSELINUX="
-================================================================================\n
-Do you want to enable SELinux(Required for Android 4.4 or later)\n
-Flags(s) : () \n
-Other Names : \n
---------------------------------------------------------------------------------\n
+================================================================================
+Do you want to enable SELinux(Required for Android 4.4 or later)(y/n)?
+Flags(s) : () 
+Other Names : 
+--------------------------------------------------------------------------------
 "
 DPDARM="var/arch/arm/configs/"
 DPDFEAT="var/feat/"
 DPDSUBARCH="var/sub-arch/"
 DPDANDROID="var/android/configs/"
-echo -e $INTRO
-echo -e $WANTMSM
-echo -e $WANTMSM7XXXA
-echo -e $WANTMSM7X30A
-echo -e $WANTANDROID
-echo -e $WANTANDROIDBASE
-PDANDROID=""
-echo -e $WANTANDROIDFULL
-PDANDROID=""
-echo -e $WANTSELINUX
-PDSELINUX=""
-PDDMCRYPT=""
+
